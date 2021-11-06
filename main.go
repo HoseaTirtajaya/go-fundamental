2dc65444627bec03fbd58f3289e601d18f7336ef
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2021-11-06 13:29:00 +0700

    declaring an empty variable or a filled variable

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	// var name string // TO DECLARE A VARIABLE WITH EMPTY VALUE
+	var name = "Hosea Tirtajaya" // TO DECLARE A RANDOM VALUE WITHIN VARIABLE
+	var age = 21
+
+	fmt.Println("Halo saya ", name, ". Saya berumur ", age)
+
+	// fmt.Println("NUMBERS: INI SATU"[3])
 }
